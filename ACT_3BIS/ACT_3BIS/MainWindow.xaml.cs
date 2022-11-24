92f0245f3a52544a2ec02450b607780320829b19
DMzartex <190478@site.asty-moulin.be>
2022-11-24 22:04:00 +0100

    ajout ACT3_BIS

---
+++ ACT_3BIS/ACT_3BIS/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ACT_3BIS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string typeLoge;
+        bool reservation;
+        int nbrPersonnes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedChalet(object sender, RoutedEventArgs e)
+        {
+            typeLoge = "chalet";
+        }
+
+        private void checkedTente(object sender, RoutedEventArgs e)
+        {
+            typeLoge = "tente";
+        }
+
+        private void checkedReservation(object sender, RoutedEventArgs e)
+        {
+            reservation = true;
+        }
+
+        private bool estEntier(string textuser)
+        {
+            return int.TryParse(textuser, out int nbr);
+        }
+
+        private void PreviewNbrPersonne(object sender, TextCompositionEventArgs e)
+        {
+            if (!estEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            else if(estEntier(e.Text))
+            {
+                nbrPersonnes = Int32.Parse(e.Text);
+                if (nbrPersonnes <= 6)
+                {
+                     
+                    if(((TextBox)sender).Text.IndexOf(e.Text) > -1 || nbrPersonnes == 0)
+                    {
+                        e.Handled = true;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+            
+        }
+    }
+}
